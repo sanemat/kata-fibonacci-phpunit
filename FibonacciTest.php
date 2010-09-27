dd39ba89cf4ab4614d2138e7b35d5208e7f47802
sanemat <o.gata.ken@gmail.com>
2010-09-28 02:25:04 +0900

    rename function

--- FibonacciTest.php
+++ FibonacciTest.php
@@ -2,7 +2,7 @@
 require_once 'Fibonacci.php';
 class FibonacciTest extends PHPUnit_Framework_TestCase
 {
-  public function testFail()
+  public function testZero()
   {
     $fib = new Fibonacci();
     $this->assertEquals(0, $fib->number(0), '0のとき0であること');
