ed5c87671e663a926368f993a4b3e57c7968cea1
sanemat <o.gata.ken@gmail.com>
2010-10-02 14:40:42 +0900

    use store prepare

--- Fibonacci.php
+++ Fibonacci.php
@@ -1,13 +1,18 @@
 <?php
 class Fibonacci{
+  private $stored = array();
   public function number($number)
   {
     if ($number === 0) {
+      $this->stored[$number] = 0;
       return 0;
     } elseif ($number === 1){
+      $this->stored[$number] = 1;
       return 1;
     }
-    return self::number($number - 2) + self::number($number - 1);
+    $fibonacciNumber = self::number($number - 2) + self::number($number - 1);
+    $this->stored[$number] = $fibonacciNumber;
+    return $fibonacciNumber;
   }
 }
 ?>
